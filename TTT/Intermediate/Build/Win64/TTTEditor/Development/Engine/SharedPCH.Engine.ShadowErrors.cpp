09f0ea6d5c17ff3899d4a639ed6075399b5cebd2
Michael Cardoso <50937149+Mikecar26@users.noreply.github.com>
2022-11-12 14:38:50 -0600

    finished game
    i finally finished, its embarrassing to say but i thought check for possible victory was to input index and not 2 or -2 for which player was going to win. . .

--- TTT/Intermediate/Build/Win64/TTTEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ TTT/Intermediate/Build/Win64/TTTEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,1 +1,2 @@
-#include "D:/Projects/Unreal Projects/TTT/Source/TicTacToe/TTT/Intermediate/Build/Win64/TTTEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.32.31332
+#include "M:/GitHub/AI-Unreal/TTT/Intermediate/Build/Win64/TTTEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
